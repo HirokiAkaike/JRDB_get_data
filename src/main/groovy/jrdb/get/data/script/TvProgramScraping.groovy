53a2c2bf1087a148a376ac0d6a1cc684243f6525
赤池 弘樹 <akaike@lightcafe.co.jp>
2018-07-22 18:07:31 +0900

    番組データ取り込み用バッチ

--- src/main/groovy/jrdb/get/data/script/TvProgramScraping.groovy
+++ src/main/groovy/jrdb/get/data/script/TvProgramScraping.groovy
@@ -1,12 +1,15 @@
 package jrdb.get.data.script
 
 import jrdb.get.data.base.BaseScraping
+import jrdb.get.data.dao.DataSourceFileDao
+import jrdb.get.data.dto.DataSourceFileDto
 import org.jsoup.Jsoup
 
 class TvProgramScraping extends BaseScraping {
     // 番組データURL
     def tv_program_list_page_url = "http://www.jrdb.com/member/datazip/Bac/index.html"
     def data_dir = config.data_base_dir.tv_program
+    def table_name = "jrdb_data_patch.tv_program_file"
 
     def run() {
         def tv_program_page = 番組データページへ遷移する()
@@ -23,12 +26,19 @@ class TvProgramScraping extends BaseScraping {
 
     private def 番組データ取得(conn){
         def tv_program_zip_list = conn.select('table li a')
+        def dataSourceFileDao = new DataSourceFileDao()
         tv_program_zip_list.each {
-            def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
-                    .ignoreContentType(true).execute()
-            def out = new OutputStreamWriter(new FileOutputStream(new java.io.File(data_dir + it.text())))
-            out.write(response.body())
-            out.close()
+            if(dataSourceFileDao.selectForDataSourceFileByFileName(table_name, it.text()) == null){
+                def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
+                        .ignoreContentType(true).execute()
+                println data_dir + it.text()
+                def out = new OutputStreamWriter(new FileOutputStream(new File(data_dir + it.text())))
+                out.write(response.body())
+                out.close()
+                def dto = new DataSourceFileDto()
+                dto.file_name = it.text()
+                dataSourceFileDao.insertDataSourceFile(table_name, dto)
+            }
         }
     }
 }
