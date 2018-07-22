931f06e6dbcabb55abcab41c7390f83683d6e322
赤池 弘樹 <akaike@lightcafe.co.jp>
2018-07-22 20:22:12 +0900

    成績データ取得用バッチ

--- src/main/groovy/jrdb/get/data/script/ScoreDataScraping.groovy
+++ src/main/groovy/jrdb/get/data/script/ScoreDataScraping.groovy
@@ -1,12 +1,15 @@
 package jrdb.get.data.script
 
 import jrdb.get.data.base.BaseScraping
+import jrdb.get.data.dao.DataSourceFileDao
+import jrdb.get.data.dto.DataSourceFileDto
 import org.jsoup.Jsoup
 
 class ScoreDataScraping extends BaseScraping {
     // 成績データURL
     def score_data_list_page_url = "http://www.jrdb.com/member/datazip/Sed/index.html"
     def data_dir = config.data_base_dir.score_data
+    def table_name = "jrdb_data_patch.score_data_file"
 
     def run() {
         def score_data_page = 成績データページへ遷移する()
@@ -23,12 +26,19 @@ class ScoreDataScraping extends BaseScraping {
 
     private def 成績データ取得(conn){
         def score_data_zip_list = conn.select('table li a')
+        def dataSourceFileDao = new DataSourceFileDao()
         score_data_zip_list.each {
-            def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
-                    .ignoreContentType(true).execute()
-            def out = new OutputStreamWriter(new FileOutputStream(new java.io.File(data_dir + it.text())))
-            out.write(response.body())
-            out.close()
+            if (dataSourceFileDao.selectForDataSourceFileByFileName(table_name, it.text()) == null) {
+                def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
+                        .ignoreContentType(true).execute()
+                println data_dir + it.text()
+                def out = new OutputStreamWriter(new FileOutputStream(new File(data_dir + it.text())))
+                out.write(response.body())
+                out.close()
+                def dto = new DataSourceFileDto()
+                dto.file_name = it.text()
+                dataSourceFileDao.insertDataSourceFile(table_name, dto)
+            }
         }
     }
 }
