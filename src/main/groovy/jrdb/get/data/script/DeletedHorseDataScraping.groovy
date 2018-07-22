816aab3260ee7b399c9ae3173a5ee99fa7df4eaa
赤池 弘樹 <akaike@lightcafe.co.jp>
2018-07-22 20:36:59 +0900

    抹消馬取得用バッチ

--- src/main/groovy/jrdb/get/data/script/DeletedHorseDataScraping.groovy
+++ src/main/groovy/jrdb/get/data/script/DeletedHorseDataScraping.groovy
@@ -1,12 +1,15 @@
 package jrdb.get.data.script
 
 import jrdb.get.data.base.BaseScraping
+import jrdb.get.data.dao.DataSourceFileDao
+import jrdb.get.data.dto.DataSourceFileDto
 import org.jsoup.Jsoup
 
 class DeletedHorseDataScraping extends BaseScraping {
     // 騎手データURL
     def deleted_horse_data_list_page_url = "http://www.jrdb.com/member/data/"
     def data_dir = config.data_base_dir.deleted_horse_data
+    def table_name = "jrdb_data_patch.deleted_horse_data_file"
 
     def run() {
         def deleted_horse_data_page = 騎手データページへ遷移する()
@@ -23,14 +26,20 @@ class DeletedHorseDataScraping extends BaseScraping {
 
     private def 騎手データ取得(conn){
         def deleted_horse_data_zip_list = conn.select('a')
+        def dataSourceFileDao = new DataSourceFileDao()
         deleted_horse_data_zip_list.each {
             if (it.attr("href").toString() ==~ /.*MZA.*zip$/) {
-                def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
-                        .ignoreContentType(true).execute()
-                def out = new OutputStreamWriter(new FileOutputStream(new java.io.File(data_dir +
-                        (it.attr("href") =~ /MZA.*zip$/)[0])))
-                out.write(response.body())
-                out.close()
+                if (dataSourceFileDao.selectForDataSourceFileByFileName(table_name, (it.attr("href") =~ /MZA.*zip$/)[0]) == null) {
+                    def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
+                            .ignoreContentType(true).execute()
+                    def out = new OutputStreamWriter(new FileOutputStream(new java.io.File(data_dir +
+                            (it.attr("href") =~ /MZA.*zip$/)[0])))
+                    out.write(response.body())
+                    out.close()
+                    def dto = new DataSourceFileDto()
+                    dto.file_name = (it.attr("href") =~ /MZA.*zip$/)[0]
+                    dataSourceFileDao.insertDataSourceFile(table_name, dto)
+                }
             }
         }
     }
