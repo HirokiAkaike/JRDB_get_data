d4a3072723de048af8566c3425ad760a872a7586
赤池 弘樹 <akaike@lightcafe.co.jp>
2018-07-22 17:56:27 +0900

    開催情報データ取り込み用バッチ

--- src/main/groovy/jrdb/get/data/script/HoldingRaceScraping.groovy
+++ src/main/groovy/jrdb/get/data/script/HoldingRaceScraping.groovy
@@ -2,12 +2,15 @@ package jrdb.get.data.script
 
 import jrdb.get.data.base.BaseScraping
 import jrdb.get.data.common.JrdbConstant
+import jrdb.get.data.dao.DataSourceFileDao
+import jrdb.get.data.dto.DataSourceFileDto
 import org.jsoup.Jsoup
 
 class HoldingRaceScraping extends BaseScraping {
     // 開催データURL
     def holding_race_list_page_url = "http://www.jrdb.com/member/datazip/Kab/index.html"
     def data_dir = config.data_base_dir.holding_race
+    def table_name = "jrdb_data_patch.holding_race_file"
 
     def run() {
         def holding_race_page = 開催データページへ遷移する()
@@ -24,12 +27,19 @@ class HoldingRaceScraping extends BaseScraping {
 
     private def 開催データ取得(conn){
         def holding_race_zip_list = conn.select('table li a')
+        def dataSourceFileDao = new DataSourceFileDao()
         holding_race_zip_list.each {
-            def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
-                    .ignoreContentType(true).execute()
-            def out = new OutputStreamWriter(new FileOutputStream(new java.io.File(data_dir + it.text())))
-            out.write(response.body())
-            out.close()
+            if(dataSourceFileDao.selectForDataSourceFileByFileName(table_name, it.text()) == null){
+                def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
+                        .ignoreContentType(true).execute()
+                println data_dir + it.text()
+                def out = new OutputStreamWriter(new FileOutputStream(new File(data_dir + it.text())))
+                out.write(response.body())
+                out.close()
+                def dto = new DataSourceFileDto()
+                dto.file_name = it.text()
+                dataSourceFileDao.insertDataSourceFile(table_name, dto)
+            }
         }
     }
 }
