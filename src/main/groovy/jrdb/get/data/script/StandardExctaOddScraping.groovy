64ff04d6b04649b04ced048344cd764736c93025
赤池 弘樹 <akaike@lightcafe.co.jp>
2018-07-22 19:42:29 +0900

    馬単オッズデータ取り込み用バッチ

--- src/main/groovy/jrdb/get/data/script/StandardExctaOddScraping.groovy
+++ src/main/groovy/jrdb/get/data/script/StandardExctaOddScraping.groovy
@@ -1,12 +1,15 @@
 package jrdb.get.data.script
 
 import jrdb.get.data.base.BaseScraping
+import jrdb.get.data.dao.DataSourceFileDao
+import jrdb.get.data.dto.DataSourceFileDto
 import org.jsoup.Jsoup
 
 class StandardExctaOddScraping extends BaseScraping {
     // 馬単基準オッズデータデータURL
     def standard_excta_odd_list_page_url = "http://www.jrdb.com/member/datazip/Ou/index.html"
     def data_dir = config.data_base_dir.standard_excta_odd
+    def table_name = "jrdb_data_patch.standard_excta_odd_file"
 
     def run() {
         def standard_excta_odd_page = 馬単基準オッズデータデータページへ遷移する()
@@ -23,12 +26,19 @@ class StandardExctaOddScraping extends BaseScraping {
 
     private def 馬単基準オッズデータデータ取得(conn){
         def standard_excta_odd_zip_list = conn.select('table li a')
+        def dataSourceFileDao = new DataSourceFileDao()
         standard_excta_odd_zip_list.each {
-            def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
-                    .ignoreContentType(true).execute()
-            def out = new OutputStreamWriter(new FileOutputStream(new java.io.File(data_dir + it.text())))
-            out.write(response.body())
-            out.close()
+            if(dataSourceFileDao.selectForDataSourceFileByFileName(table_name, it.text()) == null){
+                def response = Jsoup.connect(it.attr("abs:href")).header(headKey, headValue)
+                        .ignoreContentType(true).execute()
+                println data_dir + it.text()
+                def out = new OutputStreamWriter(new FileOutputStream(new File(data_dir + it.text())))
+                out.write(response.body())
+                out.close()
+                def dto = new DataSourceFileDto()
+                dto.file_name = it.text()
+                dataSourceFileDao.insertDataSourceFile(table_name, dto)
+            }
         }
     }
 }
