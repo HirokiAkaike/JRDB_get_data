f15e88a2d7b37b48da42e35c5b440e49608309b6
赤池 弘樹 <akaike@lightcafe.co.jp>
2018-07-29 14:08:17 +0900

    ./gradlew launch -Pmain=jrdb.get.data.app.DeletedHorseDataScrapingRunner で実行可能

--- src/main/groovy/jrdb/get/data/app/DeletedHorseDataScrapingRunner.groovy
+++ src/main/groovy/jrdb/get/data/app/DeletedHorseDataScrapingRunner.groovy
@@ -5,7 +5,6 @@ import jrdb.get.data.script.DeletedHorseDataScraping
 
 class DeletedHorseDataScrapingRunner {
     static void main(String[] args) {
-        println getClass().getResource("config.groovy")
         def runner = new DeletedHorseDataScraping()
         runner.run()
     }
